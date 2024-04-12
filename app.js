897e7bc0d6c36eb8e138a4abcb35bd7ccdf5178e
chathuryareddy2212 <156229804+chathuryareddy2212@users.noreply.github.com>
2024-04-11 19:09:18 -0500

    made changes

--- app.js
+++ app.js
@@ -4,6 +4,29 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
+var passport = require('passport');
+var LocalStrategy = require('passport-local').Strategy;
+
+passport.use(new LocalStrategy(
+  function (username, password, done) {
+    Account.findOne({ username: username })
+      .then(function (user) {
+        if (err) { return done(err); }
+        if (!user) {
+          return done(null, false, { message: 'Incorrect username.' });
+        }
+        if (!user.validPassword(password)) {
+          return done(null, false, { message: 'Incorrect password.' });
+        }
+        return done(null, user);
+      })
+      .catch(function (err) {
+        return done(err)
+      })
+  })
+)
+
+
 require('dotenv').config();
 const connectionString = process.env.MONGO_CON
 mongoose = require('mongoose');
@@ -27,6 +50,14 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
+
+app.use(require('express-session')({
+  secret: 'keyboard cat',
+  resave: false,
+  saveUninitialized: false
+}));
+app.use(passport.initialize());
+app.use(passport.session())
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
@@ -42,16 +73,26 @@ app.use('/resource', resourceRouter);
 var db = mongoose.connection;
 //Bind connection to error event
 db.on('error', console.error.bind(console, 'MongoDB connection error:'));
-db.once("open", function(){
-console.log("Connection to DB succeeded")});
+db.once("open", function () {
+  console.log("Connection to DB succeeded")
+});
+
+// passport config
+// Use the existing connection
+// The Account model
+var Account =require('./models/account');
+
+passport.use(new LocalStrategy(Account.authenticate()));
+passport.serializeUser(Account.serializeUser());
+passport.deserializeUser(Account.deserializeUser());
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   next(createError(404));
 });
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
@@ -62,37 +103,40 @@ app.use(function(err, req, res, next) {
 });
 // We can seed the collection if needed on
 
-async function recreateDB(){
-// Delete everything
-await Tree.deleteMany();
-let instance1 = new
-Tree({tree_name:"Japanese Maple",lifespan:50,leafcolor:"Red"});
-instance1.save().then(doc=>{
-console.log("First object saved")}
-).catch(err=>{
-console.error(err)
-});
+async function recreateDB() {
+  // Delete everything
+  await Tree.deleteMany();
+  let instance1 = new
+    Tree({ tree_name: "Japanese Maple", lifespan: 50, leafcolor: "Red" });
+  instance1.save().then(doc => {
+    console.log("First object saved")
+  }
+  ).catch(err => {
+    console.error(err)
+  });
 
-let instance2 = new
-Tree({tree_name:"Ginkgo",lifespan:1000,leafcolor:"golden yellow"});
-instance2.save().then(doc=>{
-console.log("Second object saved")}
-).catch(err=>{
-console.error(err)
-});
+  let instance2 = new
+    Tree({ tree_name: "Ginkgo", lifespan: 1000, leafcolor: "golden yellow" });
+  instance2.save().then(doc => {
+    console.log("Second object saved")
+  }
+  ).catch(err => {
+    console.error(err)
+  });
 
-let instance3 = new
-Tree({tree_name:"Red Maple",lifespan:100,leafcolor:"orange"});
-instance3.save().then(doc=>{
-console.log("Third object saved")}
-).catch(err=>{
-console.error(err)
-});
+  let instance3 = new
+    Tree({ tree_name: "Red Maple", lifespan: 100, leafcolor: "orange" });
+  instance3.save().then(doc => {
+    console.log("Third object saved")
+  }
+  ).catch(err => {
+    console.error(err)
+  });
 
 
 }
 let reseed = true;
-if (reseed) {recreateDB();}
+if (reseed) { recreateDB(); }
 
 
 module.exports = app;
